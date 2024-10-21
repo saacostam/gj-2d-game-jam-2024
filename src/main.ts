e17e3fbc5d2df79842f02cac7cfe835d3477c97b
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-10-20 23:56:29 -0500

    feat: setup background color from color palette

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { Color, Engine } from 'excalibur'
+import { Engine } from 'excalibur'
 import './style.css'
 import { GAME_CONFIG } from './modules/config'
 import { MainScene, SceneKey } from './modules/scenes'
 import { loader } from './modules/assets'
 import { gameOverCounterHtmlElement } from './modules/ui'
 import { UrlUtils } from './modules/url'
+import { ColorPalette } from './modules/colors'
 
 gameOverCounterHtmlElement.innerText = `❌ GAME OVERS: ${UrlUtils.getLostCount().toString()}`
 
@@ -12,7 +13,7 @@ const game = new Engine({
   width: GAME_CONFIG.WIDTH * 3,
   height: GAME_CONFIG.HEIGHT * 3,
   resolution: { width: GAME_CONFIG.WIDTH, height: GAME_CONFIG.HEIGHT },
-  backgroundColor: Color.Transparent,
+  backgroundColor: ColorPalette.Black,
   pixelArt: true,
   pixelRatio: 2,
   antialiasing: false,
--- src/modules/colors/colors.ts
+++ src/modules/colors/colors.ts
@@ -4,4 +4,5 @@ export const ColorPalette = {
   RedAlpha(a: number) {
     return new Color(203, 94, 92, a)
   },
+  Black: new Color(31, 32, 37),
 }
